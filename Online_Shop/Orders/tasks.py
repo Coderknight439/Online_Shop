e65251e79bb415e514cd69c27a9c5fa8712fdbb5
Mahadi <mahadirony.rony@gmail.com>
2019-07-11 00:30:08 +0600

    order

---
+++ Online_Shop/Orders/tasks.py
@@ -0,0 +1,15 @@
+from celery import task
+from django.core.mail import send_mail
+from .models import Order
+from django.conf import settings
+
+@task
+def order_created(order_id):  # Task to send e-mail when order is successfully created
+	order = Order.objects.get(id=order_id)
+	subject = 'Order No. {}'.format(order.id)
+	message = 'Dear {}, \n \n You have successfully placed an order. \ Your order id is {}'.format(order.first_name, order.id)
+	mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
+	return mail_sent
+
+
+
