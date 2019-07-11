6ae1b6bf6791eb3a57f2bb87d3e62cbfdf67311e
Mahadi <mahadirony.rony@gmail.com>
2019-07-11 12:08:01 +0600

    tasks

--- Online_Shop/Orders/tasks.py
+++ Online_Shop/Orders/tasks.py
@@ -7,8 +7,8 @@ from django.conf import settings
 def order_created(order_id):  # Task to send e-mail when order is successfully created
 	order = Order.objects.get(id=order_id)
 	subject = 'Order No. {}'.format(order.id)
-	message = 'Dear {}, \n \n You have successfully placed an order. \ Your order id is {}'.format(order.first_name, order.id)
-	mail_sent = send_mail(subject, message, settings.EMAIL_HOST_USER, [order.email])
+	message = 'Dear {}, \n \n You have successfully placed an order. Your order id is {}'.format(order.first_name, order.id)
+	mail_sent = send_mail(subject, message, 'mahadirony.rony@gmail.com', [order.email], fail_silently=False)
 	return mail_sent
 
 
